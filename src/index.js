0b6c96ed150b37d1bd14487896d7ef4a719ad573
Zoya Pleskatsevich <z.pleskatsevich@godeltech.com>
2020-06-29 17:12:49 +0300

    add rules in the webpack-config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello from Zoya!");
+import fooString from "./foo";
+import bar from "./bar-cjs";
+import "./logo-on-white-bg.jpg";
+
+console.log(fooString, bar);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,49 @@
 const path = require("path");
+const ExamplePlugin = require("./ExamplePlugin.js");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.join(__dirname, "build"),
+    path: path.join(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.jpe?g$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [new ExamplePlugin()],
 };
+
+// rules: [
+//   {
+//     test: /\.js$/,
+//     use: "babel-loader",
+//   },
+//   {
+//     test: /\.css$/,
+//     use: [
+//       {
+//         loader: "style-loader",
+//       },
+//       {
+//         loader: "css-loader",
+//       },
+//     ],
+//   },
+//   {
+//     test: /\.jpeg$/,
+//     use: [
+//       {
+//         loader: "url-loader",
+//         options: {
+//           limit: 10000,
+//         },
+//       },
+//     ],
+//   },
+// ]
